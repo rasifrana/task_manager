6890bbcfef6149af0e8b22a16448ee8b9866b49d
Asif Rana <rasifrana@hotmail.com>
2020-02-02 20:02:24 +0000

    git repo initiated

---
+++ .theia/app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "<h1>Hello World</h1>"
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=os.environ.get("PORT"),
+            debug=True
+    )
