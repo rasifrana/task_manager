4a5f5c3768708fec72f58bea43b4880e84e6529f
Asif Rana <rasifrana@hotmail.com>
2020-02-28 18:51:56 +0000

    update link wired up

--- app.py
+++ app.py
@@ -13,13 +13,32 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue;text-align:center;'>Hello World</h1>"
+    return "<h1 style='color:blue;text-align:center;'>Hellooo World</h1>"
 
 # get tasks route
 @app.route("/get_tasks")
 def get_tasks():
     return render_template("tasks.html", tasks= mongo.db.tasks.find())
 
+# Add task route
+@app.route("/add_task")
+def add_task():
+    return render_template("addtask.html", catagories= mongo.db.catagories.find())
+
+
+# Post a task on url
+@app.route("/insert_task", methods=["POST"])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for("get_tasks"))
+
+
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task = mongo.db.tasks.find_one({"_id:": ObjectId(task_id)})
+    all_catagories = mongo.db.catagories.find()
+    return render_template("edittask.html", task= the_task, catagories = all_catagories)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
