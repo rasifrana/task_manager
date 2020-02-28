5cb812f13806d892c869f8a25aca6d76a6b70ad3
Asif Rana <rasifrana@hotmail.com>
2020-02-28 22:34:21 +0000

    edit button and delete button functionality added

--- app.py
+++ app.py
@@ -20,13 +20,13 @@ def hello():
 def get_tasks():
     return render_template("tasks.html", tasks= mongo.db.tasks.find())
 
-# Add task route
+# get Add task route
 @app.route("/add_task")
 def add_task():
     return render_template("addtask.html", catagories= mongo.db.catagories.find())
 
 
-# Post a task on url
+# insert task / Post a task on url
 @app.route("/insert_task", methods=["POST"])
 def insert_task():
     tasks = mongo.db.tasks
@@ -34,11 +34,34 @@ def insert_task():
     return redirect(url_for("get_tasks"))
 
 
+# Edit a Taks page
 @app.route('/edit_task/<task_id>')
 def edit_task(task_id):
-    the_task = mongo.db.tasks.find_one({"_id:": ObjectId(task_id)})
+    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     all_catagories = mongo.db.catagories.find()
     return render_template("edittask.html", task= the_task, catagories = all_catagories)
+
+
+# Update task route
+@app.route("/update_task/<task_id>", methods=["POST"])
+def update_task(task_id):
+    tasks = mongo.db.tasks
+    tasks.update( {'_id': ObjectId(task_id)},
+    {
+        'task_name':request.form.get('task_name'),
+        'category_name':request.form.get('category_name'),
+        'task_description': request.form.get('task_description'),
+        'due_date': request.form.get('due_date'),
+        'is_urgent':request.form.get('is_urgent')
+    })
+    return redirect(url_for("get_tasks"))
+
+
+# Delete a task
+@app.route("/delete_task/<task_id>")
+def delete_task(task_id):
+    mongo.db.tasks.remove({'_id': ObjectId(task_id)})
+    return redirect(url_for("get_tasks"))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
