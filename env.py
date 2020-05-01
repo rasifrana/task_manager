4b2b2819bf5256c0c45020f1bb9162a72c8df32a
Asif Rana <rasifrana@hotmail.com>
2020-05-01 00:18:22 +0000

    json files added to gitignore

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ['MONGO_DBNAME'] = 'task_manager'
+os.environ['MONGO_URI'] = "mongodb+srv://root:Dublin15@mycluster-sw86k.mongodb.net/task_manager?retryWrites=true&w=majority"
