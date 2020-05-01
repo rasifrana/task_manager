0d160a8f066bbd8506f082057009b45bac987edd
Asif Rana <rasifrana@hotmail.com>
2020-05-01 00:33:22 +0000

    env file updated

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-os.environ['MONGO_DBNAME'] = 'task_manager'
-os.environ['MONGO_URI'] = "mongodb+srv://root:Dublin15@mycluster-sw86k.mongodb.net/task_manager?retryWrites=true&w=majority"
